4d7178cd9cfd7077e862362d1a28a0b434f817a0
Jackson Bunting <jbunting@tamu.edu>
2023-02-09 11:19:11 -0600

    Add empirical vignette to documentation

--- docs/src/index.md
+++ docs/src/index.md
@@ -9,3 +9,93 @@ homogeneity_test_fn
 ```@docs
 euler_algorithm
 ```
+
+
+## Empirical vignette
+
+The following code partially replicates the empirical application in Bugni; Bunting and Ura (2023).
+
+```@setup init
+import Random
+using HomogeneityTestBBU
+Random.seed!(1)
+S = [ 13  13  13  13  11   8   8   8   7   7   6   6   9   9   9   8   8   9   9;
+ 15  20  19  19  20  20  14  15  21  20  20  20  21  21  19  19  19  20  20;
+ 14  14  14  14  14  15  15  15  15  15  15  15  13  13  12  12  12  12  12;
+ 12  12  12  12  12  12  12  12  12  12  13  13  13  13  13  13  12  13  13;
+ 32  36  36  37  38  38  38  36  32  33  33  35  36  35  33  33  33  35  34;
+ 13  18  16  16  16  17  17  17  14  15  15  14  14  15  13  13  13  13  13;
+  7  10  10  10  10   8   8   9  11  11  11  10  10  10   9  10  10  10  10;
+ 17  17  18  18  18  18  18  16  16  16  16  15  15  15  14  14  14  14  14;
+ 14  13  13  11  11  11  11  12  12  12  12  11  11  11  10  11  11  11  10;
+ 23  23  22  24  24  24  24  24  23  22  22  20  20  20  19  20  20  20  20;
+  8  10  10  10  10  10  10  10  10   8   8   8   8   8   8   8   9   9  10;
+ 13  13  13  12  12   7   7   9  12  12  12  13  13  13  12  12  12  12  12;
+ 14  14  14  14  14  14  14  14  14  14  14  14  14  14  13  12  12  13  13;
+ 11  11  11  11  11  11  11  11  11  11   9   9   9   9   8   8   8   8   8;
+ 11  11  11   9   9   9   8   9   9   6   6   6   6   6   5   6   6   6   6;
+ 17  18  18  18  18  17  18  18  18  18  18  18  18  18  16  16  16  16  17;
+ 28  25  23  25  25  23  23  23  23  23  23  23  24  24  22  22  22  20  20;
+ 22  21  21  22  21  21  22  22  22  21  22  21  21  22  20  20  20  20  21;
+ 19  19  15  17  17  17  17  17  17  17  17  17  18  18  14  14  14  14  15;
+ 11  12  12  12  11  11  11   9   8   8   8   8   8   5   5   5   5   5   5;
+ 33  32  27  30  30  30  30  31  31  31  31  33  33  34  30  31  30  31  31;
+ 12  12  12  11  12  12  12  12  12  12  12  13  12  12  11  11  11  11  12;
+ 47  46  45  51  48  47  47  44  43  39  39  39  39  39  36  36  36  36  36]
+
+function test_fn(S_data, A_data)
+
+    S_support = sort(unique(S_data));
+    S_data = hcat(S_data, A_data[:,end]);
+    outcome_support = sort(unique(S_data[:, 2:end]))
+  
+    S_cols = size(S_data, 2);
+    
+    T_P = Array{Union{Missing, Float64}}(missing, size(S_data,1), size(S_support, 1), size(outcome_support, 1));
+    T_P_star = Array{Union{Missing, Float64}}(missing, size(S_data,1),size(S_support, 1), size(outcome_support, 1));
+    P_d = Array{Float64}(undef, size(S_support, 1), size(outcome_support, 1));
+    
+    for j2 in eachindex(S_support)
+        for j3 in eachindex(outcome_support)
+            tmp0 = (S_data[:,1:(S_cols-1)].==S_support[j2]);
+            if sum(tmp0)>0
+                P_d[j2,j3] = sum((S_data[:,2:S_cols][tmp0].==outcome_support[j3])) / sum(tmp0);
+            end
+            if P_d[j2,j3] > 0 # If P_d==0, then tpstar contribution is \divide 0, and wjd is \infty
+                for j1 in axes(S_data, 1)
+                    tmp1 = (S_data[j1,1:(S_cols-1)].==S_support[j2]);
+                    if sum(tmp1)>0 # If tmp1 is zero, then Pjd is \divide 0
+                        P_j_d = sum((S_data[j1,2:S_cols][tmp1] .== outcome_support[j3])) ./   sum(tmp1);
+                        W_j_d = sum(tmp1) / P_d[j2,j3];
+                        T_P[j1,j2,j3] = W_j_d * (P_j_d - P_d[j2,j3])^2;
+                        if (P_j_d > 0)
+                            T_P_star[j1, j2, j3] = 2 *  W_j_d * P_d[j2,j3] * P_j_d * log(P_j_d / P_d[j2,j3]);
+                        end
+                    end
+                end
+            end
+        end
+    end
+
+    T_P = sum(skipmissing(T_P));
+    T_P_star = sum(skipmissing(T_P_star));
+    
+    return T_P, T_P_star
+end
+
+```
+
+```@example init
+S_pre = S[:,1:10];
+A_pre = S[:,2:11]
+TS = test_fn(S_pre, A_pre)
+println(TS)
+```
+```@example init
+t = homogeneity_test_fn(X = (S_pre, A_pre),
+    test_stat_fn = test_fn,
+    K=10000,
+    A_trivial=true)
+println(t.Pvalue)
+```
+
--- src/homogeneity_test.jl
+++ src/homogeneity_test.jl
@@ -8,19 +8,21 @@ This function implements the homogeneity test of Bugni, Bunting and Ura (2023).
 - `X::Tuple`: The data in the form of tuple ``X=(S, A)``, where A may be empty.
 - `test_stat_fn::Function`: A function that takes the arguments ``S`` and ``A`` (or ``S`` only, if ``A=[~]``) and returns a (vector of) test statistics.
 - `K::Int`: An integer indicating the length of the MCMC chain. Defaults to 10,000.
-- `verbose::Boolean`: A Boolean indicating if additional output is to be returned. Defaults to false.  
+- `verbose::Vector{String}`: Possible elements of the vector are `"P value"`, `"Test stat"`, and `"X^K"`. Defaults to empty.
+- `A_trivial::Boolean`: A Boolean indicating whether ``S'=A``. Defaults to false.
 
 ## Values
 - `Pvalue`: The test's p-value (Bugni, Bunting and Ura (2023), equation (4)).
-- `test_stat`: The test statistic computed on ``X`` (optional).
 - `Pvalue_chain`: The p-value computed at each of ``K`` steps (optional).
-- `test_stat_chain`: The test statistic computed at each of ``K`` steps (optional).
+- `test_stat_chain`: The test statistic computed on ``X`` and of ``K`` steps (optional).
+- `X_K': The tuple containing the the permuted data (optional).
 """
 function homogeneity_test_fn(;
     X::Tuple, ## The data
     test_stat_fn::Function,  ## The test statistic
     K::Int=10000, ## The length of the MCMC chain to generate
-    verbose::Bool=false 
+    verbose::Vector{String} = [],
+    A_trivial::Bool = false
     )
 
     S_data, A_data = X
@@ -78,17 +80,22 @@ function homogeneity_test_fn(;
         if !S_only
             for ind in axes(unique(SS_list, dims=1), 1)
                 ss_ind = unique(SS_list, dims=1)[ind, :]'
-                ss_perm_ind = BitArray(undef, size(S_data_permuted))
-                S_perm_temp = hcat(S_data_permuted, zeros(Int, size(S_data_permuted, 1)))
-                for i in axes(ss_perm_ind, 1)
-                    for j in axes(ss_perm_ind, 2)
-                        ss_perm_ind[i,j] = all(S_perm_temp[i,j .+ [0,1]]' .== ss_ind)
+                if (A_trivial) || (A_trivial && ss_ind[2]==0)
+                    ss_perm_ind = BitArray(undef, size(S_data_permuted))
+                    S_perm_temp = hcat(S_data_permuted, zeros(Int, size(S_data_permuted, 1)))
+                    for i in axes(ss_perm_ind, 1)
+                        for j in axes(ss_perm_ind, 2)
+                            ss_perm_ind[i,j] = all(S_perm_temp[i,j .+ [0,1]]' .== ss_ind)
+                        end
                     end
-                end
 
-                ss_ind = all(SS_list .== ss_ind, dims= 2)
-                ss_ind = reshape(ss_ind, size(S_data))
-                A_data_permuted[ss_perm_ind] = Random.shuffle(A_data[ss_ind])
+                    ss_ind = all(SS_list .== ss_ind, dims= 2)
+                    ss_ind = reshape(ss_ind, size(S_data))
+                    A_data_permuted[ss_perm_ind] = Random.shuffle(A_data[ss_ind])
+                end
+            end
+            if A_trivial
+                A_data_permuted[:,1:(end-1)] = S_data_permuted[:,2:end]
             end
         end
         if S_only
@@ -99,24 +106,29 @@ function homogeneity_test_fn(;
     end
 
     pvalue = fill(NaN, numb_statistics);
-    pvalue_eachk = fill(NaN, K, numb_statistics)
-
     for ind in 1:numb_statistics
         pvalue[ind] = sum(perm_TestStat[:,ind] .>= test_stat[ind])/K; 
-        if verbose
+    end
+
+    if "p value" ∈ lowercase.(verbose)
+        pvalue_eachk = fill(NaN, K, numb_statistics)
+        for ind in 1:numb_statistics
             for ind2 in 1:K
                 pvalue_eachk[ind2, ind] = sum(perm_TestStat[1:ind2, ind] .>= test_stat[ind]) / ind2
             end
         end
     end
 
-
-    if verbose
-        return (Pvalue = pvalue, 
-                test_stat = test_stat, 
-                Pvalue_chain = pvalue_eachk,
-                test_stat_chain = perm_TestStat)
-    else
-        return Pvalue = pvalue
+    t = (Pvalue = pvalue,)
+    if "p value" ∈ lowercase.(verbose)
+        t =  merge(t, (;Pvalue_chain = pvalue_eachk))
     end
+    if "test stat" ∈ first.(lowercase.(verbose), 9)
+        t =  merge(t, (;test_stat_chain = vcat([test_stat[i] for i=1:numb_statistics]', perm_TestStat)))
+    end
+    if "x^k" ∈ lowercase.(verbose)
+        t = merge(t, (;X_K = (S_data_permuted, A_data_permuted)))
+    end
+
+    return t
 end
